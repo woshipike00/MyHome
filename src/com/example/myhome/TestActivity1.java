867364b006b20528efbfdcb68fb20d428ade65a6
pike <rfzhunju@gmail.com>
2013-04-14 21:45:26 +0800

    add workflow

--- src/com/example/myhome/TestActivity1.java
+++ src/com/example/myhome/TestActivity1.java
@@ -1,8 +1,12 @@
 package com.example.myhome;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -28,8 +32,19 @@ public class TestActivity1 extends Activity{
 				// TODO Auto-generated method stub
 				/*setResult(RESULT_OK,new Intent());
 				TestActivity1.this.finish();*/
-				
-				
+				ArrayList<HashMap<String, Object>> workflow=(ArrayList<HashMap<String, Object>>)getIntent().getSerializableExtra("workflow");
+				if(workflow.size()>0){
+					String classname=(String)workflow.get(0).get("intent");
+					workflow.remove(0);
+					
+					Intent intent=new Intent();
+					intent.putExtra("workflow", workflow);
+					intent.setClassName(TestActivity1.this, classname);
+					startActivity(intent);
+				}
+				else {
+					Log.v("myhome", "workflows size=0");
+				}
 			}
 			
 		});
